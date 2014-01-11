9021243183882b7d00f0f67e13c3125af53be6b2
thomas <tmaurice59@gmail.com>
2014-01-11 12:50:45 +0100

    Initial commit, bare file

---
+++ pypodcatcher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	  pypodder.py (PBS) : Connect to bluetooth serial devices.
+	
+	           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
+ 
+	Copyright (C) 2013 Thomas Maurice <tmaurice59@gmail.com>
+	 
+	Everyone is permitted to copy and distribute verbatim or modified
+	copies of this license document, and changing it is allowed as long
+	as the name is changed.
+	 
+		         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+		TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+	 
+	 0. You just DO WHAT THE FUCK YOU WANT TO.
+	 
+"""
+
+__author__ = "Thomas Maurice"
+__copyright__ = "Copyright 2013, Thomas Maurice"
+__license__ = "WTFPL"
+__version__ = "0.2"
+__maintainer__ = "Thomas Maurice"
+__email__ = "tmaurice59@gmail.com"
+__status__ = "Development"
